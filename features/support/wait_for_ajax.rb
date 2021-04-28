6355db6ed736af4c58fd5156755135a0a678b134
Axel Bolle <abolle@itrsgroup.com>
2021-04-28 09:56:32 +0200

    Refactor wait for ajax
    Signed-off-by: Axel Bolle <abolle@itrsgroup.com>

--- features/support/wait_for_ajax.rb
+++ features/support/wait_for_ajax.rb
@@ -1,21 +1,22 @@
 module WaitForAjax
   extend self
   
-  #Helper Functions
-  def wait_for_ajax
-    require "timeout"
-    Timeout.timeout(Capybara.default_max_wait_time) do
-      loop until finished_all_ajax_requests?
+#Helper Functions
+def wait_for_ajax
+  attempts = 20
+  while attempts >= 0 and not finished_all_ajax_requests? do
+    attempts -= 1
+    # It's had 5 seconds to load, maybe it's stuck? Reload and keep trying.
+    if attempts % 5 == 0 then
+      Capybara.page.evaluate_script("window.location.reload()")
     end
+    sleep(1)
   end
-  
-  def finished_all_ajax_requests?
-    if Capybara.page.evaluate_script('typeof(jQuery)') == 'function'
-      Capybara.page.evaluate_script('jQuery.active').zero?
-    else
-      return true    
-    end
-  end
+end
+
+def finished_all_ajax_requests?
+  Capybara.page.evaluate_script('document.readyState === "complete" && typeof(jQuery) === "function" && jQuery.active === 0')
+end
 
   end
   
